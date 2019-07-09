8b85fe4f290b29d628841eb211085b5e6a13c243
Tom Gousby <gousbyjr@gmail.com>
2019-07-09 13:34:15 -0400

    working on auth in rails

--- canidate_app/src/Components/AllCanidates.js
+++ canidate_app/src/Components/AllCanidates.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom"
+
 
 class AllCanidates extends Component {
     constructor(props) {
         super(props);
         
+        this.state = {
+            canidatesArray: []
+        };
+    }
+
+    async componentDidMount() {
+        const response = await axios.get("/c")
     }
     
     render() {
--- canidate_app/src/Components/SignIn.js
+++ canidate_app/src/Components/SignIn.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 
 class SignInPage extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            email: "",
+            password: "",
+        };
+    }
+    
     render() {
         return (
             <div>
-                
+                <Link className="" to="/home"></Link>
             </div>
         );
     }
